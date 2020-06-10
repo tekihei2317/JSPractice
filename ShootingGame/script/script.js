03ff0649611fa3710cd9929e25fd7030ac6a60d8
tekihei2317 <tekihei2317@gmail.com>
2020-06-10 15:09:38 +0900

    feat: 自機を動かす機能を追加

--- ShootingGame/script/script.js
+++ ShootingGame/script/script.js
@@ -5,6 +5,10 @@
   let util = null;
   let ctx = null;
   let image = null;
+  let startTime = null;
+
+  let viperX = CANVAS_WIDTH / 2;
+  let viperY = CANVAS_HEIGHT / 2;
 
   /**
    * ページのロード完了時に実行
@@ -17,17 +21,46 @@
     util.imageLoader("./image/viper.png", (loadedImage) => {
       image = loadedImage;
       initialize();
+      eventSetting();
+      startTime = Date.now();
       render();
     });
   });
 
+  /**
+   * Canvasの大きさを設定
+   */
   function initialize() {
     canvas.width = CANVAS_WIDTH;
     canvas.height = CANVAS_HEIGHT;
   }
 
+  /**
+   * イベントを設定する
+   */
+  function eventSetting() {
+    window.addEventListener("keydown", (event) => {
+      if (event.key === "ArrowLeft") {
+        viperX -= 10;
+      } else if (event.key === "ArrowRight") {
+        viperX += 10;
+      } else if (event.key === "ArrowUp") {
+        viperY -= 10;
+      } else if (event.key === "ArrowDown") {
+        viperY += 10;
+      }
+    });
+  }
+
+  /**
+   * 描画処理を行う
+   */
   function render() {
+    // 描画前に全体をグレーで塗りつぶす
     util.drawRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT, "#eee");
-    ctx.drawImage(image, 100, 100);
+
+    let currentTime = (Date.now() - startTime) / 1000;
+    ctx.drawImage(image, viperX, viperY);
+    requestAnimationFrame(render);
   }
 })();
